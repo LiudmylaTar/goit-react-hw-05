33c329addbfc1b694bcda2099790ebc9bc82c6ae
Liudmyla Taran <taran.lyudok@gmail.com>
2025-05-15 11:06:47 +0300

    Update movies-api.js

--- src/movies-api.js
+++ src/movies-api.js
@@ -1,18 +1,6 @@
 import axios from "axios";
 
-const url =
-  "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
-const options = {
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzlkYzYwOTVkNjgzMDExZWRhMDdlNWRhMDA2YWE4MCIsIm5iZiI6MTc0NzExODQ0Mi40NzQsInN1YiI6IjY4MjJlOTZhNzNiMzVhMDcxMDdlODE2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uV8mbGkCYQRGm6_nLyMYoQ8DmN36gNJNk_8VReaA58A",
-  },
-};
 
-axios
-  .get(url, options)
-  .then((response) => console.log(response.data))
-  .catch((error) => console.error(error));
 
 // // export const fetchGallery = async (typeImg, currentPage) => {
 // //   const response = await axios.get("/search/photos", {
