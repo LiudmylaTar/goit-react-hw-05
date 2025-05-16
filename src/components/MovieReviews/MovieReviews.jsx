dcc7b2305ed6e6dca1d1da3354ff60156da03d74
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-05-16 21:57:50 +0300

    HW-5 rev-1 finish

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -2,15 +2,22 @@ import { useEffect, useState } from "react";
 import css from "./MovieReviews.module.css";
 import { useParams } from "react-router-dom";
 import { fetchReviews } from "../../movies-api";
+import Loader from "../Loader/Loader";
 
 export default function MovieReviews() {
   const { filmId } = useParams();
   const [reviews, setReviews] = useState([]);
-
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [expandedIds, setExpandedIds] = useState([]);
 
   useEffect(() => {
-    fetchReviews(filmId).then((reviews) => setReviews(reviews));
+    setIsLoading(true);
+    setIsError(false);
+    fetchReviews(filmId)
+      .then((reviews) => setReviews(reviews))
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
   }, [filmId]);
 
   const toggleExpand = (id) => {
@@ -20,23 +27,31 @@ export default function MovieReviews() {
   };
   return (
     <div>
-      {reviews.length > 0 &&
-        reviews.map((review) => {
-          const isExpanded = expandedIds.includes(review.id);
-          return (
-            <div key={review.id} className={css.reviewItem}>
-              <h3 className={css.reviewAuthor}>Author: {review.author}</h3>
-              <p
-                className={`${css.reviewContent} ${
-                  isExpanded ? css.expanded : ""
-                }`}
-                onClick={() => toggleExpand(review.id)}
-              >
-                {review.content}
-              </p>
-            </div>
-          );
-        })}
+      {isLoading && <Loader loading={isLoading} />}
+      {isError && <p>Something went wrong while loading reviews.</p>}
+
+      {!isLoading &&
+        !isError &&
+        (reviews.length > 0 ? (
+          reviews.map((review) => {
+            const isExpanded = expandedIds.includes(review.id);
+            return (
+              <div key={review.id} className={css.reviewItem}>
+                <h3 className={css.reviewAuthor}>Author: {review.author}</h3>
+                <p
+                  className={`${css.reviewContent} ${
+                    isExpanded ? css.expanded : ""
+                  }`}
+                  onClick={() => toggleExpand(review.id)}
+                >
+                  {review.content}
+                </p>
+              </div>
+            );
+          })
+        ) : (
+          <p>No reviews yet. Be the first to leave one!</p>
+        ))}
     </div>
   );
 }
